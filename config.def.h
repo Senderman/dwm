76de9456322bd2d274d626efb7e9dc866a3ce08c
Fyodor Doletov <doletov.fyodor@yandex.ru>
2021-11-14 01:38:59 +0300

    update comments

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int notifyfd           = 0;        /* file descriptor for readiness notification. Must be greater than 2 to work. 0 means no fd */
+static const int notifyfd           = 0;        /* file descriptor for readiness notification. Must be greater than 3 to work. 0 means no fd */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
