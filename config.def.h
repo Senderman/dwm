5b0b27fae7343a4b7ed087fc41ee63f9f98dc44f
Fyodor Doletov <doletov.fyodor@yandex.ru>
2025-03-17 03:21:28 +0300

    remove steam from 1 << 8 tag

--- config.def.h
+++ config.def.h
@@ -48,7 +48,6 @@ static const Rule rules[] = {
 	{ "Telegram",             NULL,       NULL,                   1 << 1,       0,        -1 },
 	{ "discord",              NULL,       NULL,                   1 << 1,       0,        -1 },
 	{ "Virt-manager",         NULL,       NULL,                   1 << 6,       0,        -1 },
-	{ "steam",                NULL,       NULL,                   1 << 8,       0,        -1 },
 	{ "steam",                NULL,       "Friends List",         1 << 8,       1,        -1 },
 	{ "steam",                NULL,       "Special Offers",       1 << 8,       1,        -1 },
 	{ "jetbrains-idea-ce",    NULL,       NULL,                   1 << 3,       0,        -1 },
